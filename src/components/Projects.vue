28ae3e9e2dc32add2770abd1795e9496065beab4
BunnyTheLifeguard <36411819+BunnyTheLifeguard@users.noreply.github.com>
2021-09-02 21:00:48 +0200

    Gh links & cleanup

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -28,9 +28,9 @@
           fade-delay
         "
       >
-        An excerpt of my last and current projects, clicking a tile opens the
+        An excerpt of my most recent projects, clicking a tile opens the
         project's website in a new browser tab. <br />
-        For the source code feel free to visit my
+        For more details and source code feel free to visit my
         <a
           href="https://github.com/BunnyTheLifeguard"
           target="_blank"
@@ -48,14 +48,20 @@
             fadeup
             fade-delay
           "
-          >This site is optimized for desktop PCs. If you are watching it on a
+          >This site is optimized for desktop PCs. If you are using it on a
           device with a touchscreen you will miss out on some animations.</i
         >
       </h4>
     </div>
 
     <div
-      class="flex flex-grow-0 flex-shrink-0 items-center justify-center m-0 p-0"
+      class="
+        flex-col flex-grow-0 flex-shrink-0
+        items-center
+        justify-center justify-items-center
+        m-0
+        p-0
+      "
       v-for="project in projects"
       :key="project.name"
       id="project-canvas"
@@ -154,6 +160,36 @@
           </div>
         </section>
       </a>
+      <div class="flex justify-center items-center">
+        <a
+          :href="project.git"
+          target="_blank"
+          class="
+            text-center text-phoenixYellow
+            py-5
+            px-1
+            no-underline
+            hover:underline
+          "
+        >
+          View Souce Code </a
+        ><svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          aria-hidden="true"
+          role="img"
+          class="iconify iconify--mdi p-1 ml-2"
+          width="32"
+          height="32"
+          preserveAspectRatio="xMidYMid meet"
+          viewBox="0 0 24 24"
+        >
+          <path
+            d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2z"
+            fill="#fefefe"
+          ></path>
+        </svg>
+      </div>
     </div>
   </div>
 </template>
