11c96f21dcf6c415afb8d959d57b64272be84136
BunnyTheLifeguard <36411819+BunnyTheLifeguard@users.noreply.github.com>
2021-09-03 12:30:03 +0200

    Typo

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -172,7 +172,7 @@
             hover:underline
           "
         >
-          View Souce Code </a
+          View Source Code </a
         ><svg
           xmlns="http://www.w3.org/2000/svg"
           xmlns:xlink="http://www.w3.org/1999/xlink"
